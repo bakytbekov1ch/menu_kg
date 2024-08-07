4fd3b9b412201091089e03fd5736fdfb357986a4
bakytbekov1ch <kalmamatovelibek32@gmail.com>
2024-08-07 10:44:58 +0600

    fireebas

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ function App() {
 
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          {/* <h1>Hello My World</h1> */}
         </Route>
       </Routes>
     </div>
